f268e6f94f422ed25fc2b3b3a4379de2a9789df0
Dmitry <dmitrychinyaev93@yandex.ru>
2023-01-06 22:33:26 +0300

    add method add.Handler

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,8 +1,30 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
         Server server = new Server(64, 9999);
+        server.addHandler("GET", "/resources.html", new Handler() {
+            @Override
+            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                final var filePath = Path.of(".", "public/resources.html");
+                final var mimeType = Files.probeContentType(filePath);
+                final var length = Files.size(filePath);
+                responseStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, responseStream);
+                responseStream.flush();
+            }
+        });
         server.start();
     }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -9,13 +9,14 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
-    ExecutorService executorService;
+    private Map<String, Map<String, Handler>> methodHandlers = new ConcurrentHashMap<>();
+    private ExecutorService executorService;
     int port;
 
     public Server(int threadPoolSize, int port) {
@@ -23,8 +24,19 @@ public class Server {
         this.port = port;
     }
 
-    public Server(int port){
+    public Server(int port) {
         this.port = port;
+    }
+
+    public void addHandler(String method, String way, Handler handler) {
+        Set<String> setKeys = methodHandlers.keySet();
+        if (!setKeys.contains(method)) {
+            Map<String, Handler> checkHandlers = new HashMap<>();
+            checkHandlers.put(way, handler);
+            methodHandlers.put(method, checkHandlers);
+        } else {
+            methodHandlers.get(method).put(way, handler);
+        }
     }
 
     public void start() {
@@ -47,15 +59,11 @@ public class Server {
             // read only request line for simplicity
             // must be in form GET /path HTTP/1.1
             final var requestLine = in.readLine();
-            final var parts = requestLine.split(" ");
+            List<String> parts = Arrays.asList(requestLine.split(" "));
+            Request request = new Request(parts);
+            Set<String> set = methodHandlers.keySet();
 
-            if (parts.length != 3) {
-                // just close socket
-                return;
-            }
-
-            final var path = parts[1];
-            if (!validPaths.contains(path)) {
+            if (!set.contains(request.getMethod()) || parts.size() < 3) {
                 out.write((
                         "HTTP/1.1 404 Not Found\r\n" +
                                 "Content-Length: 0\r\n" +
@@ -66,40 +74,9 @@ public class Server {
                 return;
             }
 
-            final var filePath = Path.of(".", "public", path);
-            final var mimeType = Files.probeContentType(filePath);//"application/octet-stream" "text/plain"
+            methodHandlers.get(request.getMethod()).get(request.getWay()).handle(request,out);
 
-            // special case for classic
-            if (path.equals("/classic.html")) {
-                final var template = Files.readString(filePath);
-                final var content = template.replace(
-                        "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + content.length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.write(content);
-                out.flush();
-                return;
-            }
-
-            final var length = Files.size(filePath);
-            out.write((
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            Files.copy(filePath, out);
-            out.flush();
-        } catch (
-                IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
