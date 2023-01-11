a8c1e01672cb99a16f17fd4cd051cf57b7b23ff9
Dmitry <dmitrychinyaev93@yandex.ru>
2023-01-11 17:42:09 +0300

    small changes

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,11 +1,15 @@
 package ru.netology;
 
+import org.apache.http.NameValuePair;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Request {
     private String method;
     private String way;
     private String version;
+    private List<NameValuePair> queryParams;
 
     public String getMethod() {
         return method;
@@ -29,6 +33,27 @@ public class Request {
 
     public void setVersion(String version) {
         this.version = version;
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    public void setQueryParams(List<NameValuePair> queryParams) {
+        this.queryParams = queryParams;
+    }
+
+    public List<NameValuePair> getQueryParam(String name){
+        if (queryParams == null){
+            return null;
+        }
+        List<NameValuePair> chosenQueryParam = new ArrayList<>();
+        for (NameValuePair nameValuePair : queryParams){
+            if(nameValuePair.getName().equals(name)){
+                chosenQueryParam.add(nameValuePair);
+            }
+        }
+        return chosenQueryParam;
     }
 
     public Request(List<String> request) {
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,11 +1,15 @@
 package ru.netology;
 
+import org.apache.http.client.utils.URLEncodedUtils;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
@@ -53,28 +57,35 @@ public class Server {
              final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              final var out = new BufferedOutputStream(socket.getOutputStream())
         ) {
-            // read only request line for simplicity
-            // must be in form GET /path HTTP/1.1
             final var requestLine = in.readLine();
             List<String> parts = Arrays.asList(requestLine.split(" "));
             Request request = new Request(parts);
+
+            if (request.getWay().contains("?")){
+                String [] wayParts = request.getWay().split("\\?");
+                request.setQueryParams(URLEncodedUtils.parse(
+                        new URI(request.getWay()), "UTF-8"));
+                request.setWay(wayParts[0]);
+            }
             Set<String> set = methodHandlers.keySet();
 
             if (!set.contains(request.getMethod()) || parts.size() < 3) {
-                out.write((
-                        "HTTP/1.1 404 Not Found\r\n" +
-                                "Content-Length: 0\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.flush();
-                return;
+                badRequest(out);
             }
 
             methodHandlers.get(request.getMethod()).get(request.getWay()).handle(request,out);
 
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
+    }
+    private static void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
     }
 }
